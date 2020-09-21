e35dae145b7311080471ac7cc7865b53f596d288
Lito White <leland.white@gmail.com>
2020-09-21 09:26:28 -0600

    test in progress for api call

---
+++ spec/services/route_service_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe RouteService do
+  describe 'class methods' do
+    it 'returns a list of routes' do
+      service = RouteService
+      lat =
+      long =
+      search = service.find_routes(location)
+    end
+  end
+end
