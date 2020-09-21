54e1ce40eae7e5be5382b5b8d8161b66871afad7
Lito White <leland.white@gmail.com>
2020-09-21 09:49:20 -0600

    API call successful

---
+++ app/services/route_service.rb
@@ -0,0 +1,19 @@
+class RouteService
+
+  def self.find_routes(location)
+    response = conn.get('/data/get-routes-for-lat-lon') do |req|
+      req.params['lat'] = location.lat
+      req.params['lon'] = location.long
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  private
+
+  def self.conn
+    url = 'https://www.mountainproject.com'
+    @conn ||= Faraday.new(url: url) do |req|
+      req.params['key'] = ENV['MOUNTAIN_PROJECT_API_KEY']
+    end
+  end
+end
--- spec/services/route_service_spec.rb
+++ spec/services/route_service_spec.rb
@@ -4,9 +4,20 @@ describe RouteService do
   describe 'class methods' do
     it 'returns a list of routes' do
       service = RouteService
-      lat =
-      long =
+      lat = 40.03
+      long = -105.25
+      location = OpenStruct.new(
+        lat: lat,
+        long: long
+      )
       search = service.find_routes(location)
+      expect(search).to be_an Hash
+      expect(search[:routes].count).to eq(50)
+      expect(search[:routes][0].keys).to include(:id)
+      expect(search[:routes][0].keys).to include(:name)
+      expect(search[:routes][0].keys).to include(:type)
+      expect(search[:routes][0].keys).to include(:rating)
+      expect(search[:routes][0].keys).to include(:location)
     end
   end
 end
