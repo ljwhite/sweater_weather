79b23a038003a928b93ecea5eb9018ab1271235c
Lito White <leland.white@gmail.com>
2020-09-21 21:09:44 -0600

    user story 2 partially complete

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,9 @@
+class Api::V1::UsersController < ApplicationController
+  def create
+    user = User.new(user_params)
+  end
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
+  end
+end
--- spec/requests/api/v1/user_request_spec.rb
+++ spec/requests/api/v1/user_request_spec.rb
@@ -2,10 +2,9 @@ require 'rails_helper'
 
 describe 'User API' do
   it 'should create a new user' do
-    # params_data = :params => {email: 'email', password: 'abc', password_confirmation: 'abc'}
 
     post '/api/v1/users', params: {email: 'email', password: 'abc', password_confirmation: 'abc'}
-    json = JSON.parse(response.body, symbolize_names: true)
+    # json = JSON.parse(response.body, symbolize_names: true)
     expect(response).to be_successful
     user = User.last
 
