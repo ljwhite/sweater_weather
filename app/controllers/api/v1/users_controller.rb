3039996c72992dbb8b2ca5a4c7d62610c311dcde
Lito White <leland.white@gmail.com>
2020-09-22 14:33:47 -0600

    User story 2 complete

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,14 +1,12 @@
 class Api::V1::UsersController < ApplicationController
   def create
     user = User.new(user_params)
-    
     if user.save
-      render json: UserSerializer.new(user), status: 200
+      render json: UserSerializer.new(user), status: 201
     else
       render json: user.errors, status: :unprocessable_entity
     end
   end
-
   def user_params
     params.permit(:email, :password, :password_confirmation)
   end
--- spec/requests/api/v1/user_request_spec.rb
+++ spec/requests/api/v1/user_request_spec.rb
@@ -2,12 +2,30 @@ require 'rails_helper'
 
 describe 'User API' do
   it 'should create a new user' do
-
     expect {
       post '/api/v1/users', params: {email: 'email', password: 'abc', password_confirmation: 'abc'}
     }.to change{User.count}.by(1)
-    # json = JSON.parse(response.body, symbolize_names: true)
+    json = JSON.parse(response.body, symbolize_names: true)
     expect(response).to be_successful
-    user = User.last
+    expect(response.status).to eq(201)
+    expect(response.body).not_to include('abc')
+  end
+  it "will return a 400-level status if passwords don't match " do
+    expect {
+      post '/api/v1/users', params: {email: 'email', password: 'abc', password_confirmation: 'abcd'}
+    }.to change{User.count}.by(0)
+    json = JSON.parse(response.body, symbolize_names: true)
+    expect(response.status).to eq(422)
+    expect(json[:password_confirmation][0]).to eq("doesn't match Password")
+  end
+  it "will return a 400-level status if email already exists" do
+    user1 = User.new({email: 'email', password: 'password' })
+    user1.save
+    expect {
+      post '/api/v1/users', params: {email: 'email', password: 'abc', password_confirmation: 'abc'}
+    }.to change{User.count}.by(0)
+    json = JSON.parse(response.body, symbolize_names: true)
+    expect(response.status).to eq(422)
+    expect(json[:email][0]).to eq("has already been taken")
   end
 end
