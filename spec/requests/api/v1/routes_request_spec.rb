75aa33234538074de38dcd837cd6946a49f3d693
Lito White <leland.white@gmail.com>
2020-09-22 09:58:06 -0600

    climbing route request spec complete

--- spec/requests/api/v1/routes_request_spec.rb
+++ spec/requests/api/v1/routes_request_spec.rb
@@ -5,6 +5,22 @@ describe 'Routes Controller' do
 
     get '/api/v1/climbing_routes?location=erwin,tn'
     expect(response).to be_successful
-    binding.pry 
+    expect(response.content_type).to include('application/json')
+    json = JSON.parse(response.body, symbolize_names: true)
+    expect(json[:data][:type]).to eq('climbing route')
+    expect(json[:data][:attributes].keys).to include(:location)
+    expect(json[:data][:attributes][:location]).to eq('erwin,tn')
+    expect(json[:data][:attributes].keys).to include(:forecast)
+    expect(json[:data][:attributes][:forecast].keys).to include(:summary)
+    expect(json[:data][:attributes][:forecast].keys).to include(:temperature)
+    expect(json[:data][:attributes].keys).to include(:routes)
+    expect(json[:data][:attributes][:routes]).to be_an Array
+    expect(json[:data][:attributes][:routes][0].keys).to include(:name)
+    expect(json[:data][:attributes][:routes][0].keys).to include(:type)
+    expect(json[:data][:attributes][:routes][0].keys).to include(:location)
+    expect(json[:data][:attributes][:routes][0].keys).to include(:rating)
+    expect(json[:data][:attributes][:routes][0].keys).to include(:distance_to_route)
+    expect(json[:data][:attributes][:routes][0][:location]).to be_an Array
+    expect(json[:data][:attributes][:routes][0][:distance_to_route]).to be_a Float
   end
 end
