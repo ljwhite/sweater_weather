247651d036889f30476cac58bda9411e00d07f2c
Lito White <leland.white@gmail.com>
2020-09-22 17:53:14 -0600

    Mapquest service spec complete for road trip

--- spec/services/location_service_spec.rb
+++ spec/services/location_service_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 describe LocationService do
   describe 'class methods' do
     it "returns location data" do
-      service = LocationService
       location = 'Denver,CO'
       search = LocationService.find_coordinates(location)
       expect(search).to be_a Hash
@@ -21,6 +20,16 @@ describe LocationService do
       expected_attributes.each do |attribute|
         expect(search[:results].first[:locations].first[:latLng]).to have_key(attribute)
       end
+    end
+
+    it "can find directions between two given locations" do
+      origin = "39.74,-104.99"
+      destination = "38.25,-104.61"
+      search = LocationService.find_road_trip(origin, destination)
+      expect(search).to be_a Hash
+      expect(search[:route].keys).to include(:formattedTime)
+      expect(search[:route][:formattedTime]).to be_a String
+      expect(search[:route][:formattedTime]).to eq("01:46:19")
     end
   end
 end
