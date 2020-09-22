97e0572eb2608fb0661ebdfcf6aed3e353dc9d6e
Lito White <leland.white@gmail.com>
2020-09-22 11:26:00 -0600

    Route PORO spec complete

--- spec/poros/route_poro_spec.rb
+++ spec/poros/route_poro_spec.rb
@@ -1,7 +1,24 @@
 require 'rails_helper'
 
 describe Route do
-  describe 'class methods' do
-    
+  describe 'instance methods' do
+  it 'can initialize an object given attributes' do
+      attributes =
+      {
+        name: "route_name",
+        type: "route_type",
+        rating: 5.10,
+        locaton: ["Denver", "CO"],
+        longitude: -104.98,
+        latitude: 39.74
+      }
+      route = Route.new(attributes)
+      expect(route.name).to eq(attributes[:name])
+      expect(route.type).to eq(attributes[:type])
+      expect(route.rating).to eq(attributes[:rating])
+      expect(route.location).to eq(attributes[:location])
+      expect(route.longitude).to eq(attributes[:longitude])
+      expect(route.latitude).to eq(attributes[:latitude])
+    end
   end
 end
