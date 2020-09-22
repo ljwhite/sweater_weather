1e89b47cea9a272ff4ed0bef3c8b5d316dc8a0c2
Lito White <leland.white@gmail.com>
2020-09-22 15:47:29 -0600

    User Story 3 complete and tested

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,10 +1,14 @@
 class Api::V1::SessionsController < ApplicationController
   def create
     user = User.find_by(email: sessions_params[:email])
-    if user.authenticate(sessions_params[:password])
-      render json: UserSerializer.new(user), status: 201
+    if user.nil?
+      render json: {error: "User not found"}, status: 400
     else
-      render json: {error: "Email Not Found"}, status: 422
+      if user.authenticate(sessions_params[:password])
+        render json: UserSerializer.new(user), status: 201
+      else
+        render json: {error: "Incorrect Password"}, status: 400
+      end
     end
   end
 
--- spec/requests/api/v1/login_request_spec.rb
+++ spec/requests/api/v1/login_request_spec.rb
@@ -13,7 +13,11 @@ describe 'User Login API' do
     user.save
     post '/api/v1/sessions', params: {email: 'email', password: 'wrong_password' }
     json = JSON.parse(response.body)
-    expect(response.status).to eq(422)
-    expect(json['error']).to eq('Email Not Found')
+    expect(response.status).to eq(400)
+    expect(json['error']).to eq('Incorrect Password')
+  end
+  it "if the user does not exist in the database, the login will fail " do
+    post '/api/v1/sessions', params: {email: 'wrong_email', password: 'password' }
+    json = JSON.parse(response.body)
   end
 end
