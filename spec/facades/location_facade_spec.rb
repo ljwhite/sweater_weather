08b7a92afa078e8e91da56d01fecbbd817005b8f
Lito White <leland.white@gmail.com>
2020-09-22 19:05:52 -0600

    LocationFacade class methods complete

--- spec/facades/location_facade_spec.rb
+++ spec/facades/location_facade_spec.rb
@@ -2,11 +2,16 @@ require 'rails_helper'
 
 describe LocationFacade do
   describe "class methods" do
-    it "can return a coordinate OpenStruct given a location" do
+    it "find_coordinates" do
       location = 'denver,co'
       result = LocationFacade.find_coordinates(location)
       expect(result.lat.round(2)).to eq(39.74)
       expect(result.long.round(2)).to eq(-104.98)
+    end
+    it "format_coordinates" do
+      coordinates = OpenStruct.new({lat: 39.74, long: -104.98})
+      result = LocationFacade.format_coordinates(coordinates)
+      expect(result).to eq("39.74,-104.98")
     end
   end
 end
